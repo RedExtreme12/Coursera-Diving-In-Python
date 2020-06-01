5091fdfd63e00bb71fc4754ea9b2dec0ebcc6806
Dmitriy Rogozenko <red.extreme123456@gmail.com>
2020-06-01 18:25:02 +0300

    Updated

--- week_05/task05_01/solution.py
+++ week_05/task05_01/solution.py
@@ -11,6 +11,7 @@ SERVER_RESPONSE_STATUS_OK = 'ok\n\n'
 SERVER_RESPONSE_STATUS_ERROR = 'error\nwrong command\n\n'
 
 COMMAND_GET_ALL = '*'
+COMMANDS = ('get', 'put')
 
 
 class ClientError(Exception):
@@ -20,40 +21,73 @@ class ClientError(Exception):
 class Client:
 
     def __init__(self, host, port, timeout=None):
-        self.connection_sock = socket.create_connection((host, port), timeout)
+        try:
+            self.connection_sock = socket.create_connection((host, port), timeout)
+        except socket.error:
+            raise ClientError()
+
+    def read_data(self, buffer_size):
+        data = b''
+
+        while not data.endswith(b'\n\n'):
+            try:
+                data += self.connection_sock.recv(buffer_size)
+            except socket.error:
+                raise ClientError()
+
+        data = data.decode('utf8')
+
+        if data == SERVER_RESPONSE_STATUS_ERROR:
+            raise ClientError()
+
+        return data
+
+    def send_data(self, data):
+        # Валидация данных тут!
+        try:
+            self.connection_sock.sendall(data.encode('utf8'))
+        except socket.error:
+            print('Error!')
 
     def put(self, command, metric_value, timestamp=None):
         try:
             timestamp = int(timestamp) if timestamp else int(time.time())
-            metric_value = float(metric_value)
+            # metric_value = float(metric_value)
         except ValueError as err:
             print('Invalid data format. Error:', err)
-            raise ClientError
+            raise ClientError()
 
-        msg = 'put' + command + str(metric_value) + str(timestamp) + '\n'
-        self.connection_sock.sendall(msg.encode('utf8'))
+        msg = f'put {command} {str(metric_value)} {str(timestamp)}\n'
+        self.send_data(msg)
 
-        response = self.connection_sock.recv(len(SERVER_RESPONSE_STATUS_ERROR))
+        response = self.read_data(1024)
 
         if response == SERVER_RESPONSE_STATUS_ERROR:
-            raise ClientError
+            raise ClientError()
 
-    def get(self, command):
-        self.connection_sock.sendall(command.encode('utf8'))
+    def get(self, value_metric):
 
-        response = self.connection_sock.recv(4096).decode('utf8')
+        # Формат ответа сервера: <статус ответа><\n><данные ответа><\n\n>
 
-        if not response:
-            raise ClientError
+        command = f'get {str(value_metric)}\n'
+        self.send_data(command)
 
-        values_list = response.rstrip('\n').split('\n')[1:]
+        response = self.read_data(1024)
+
+        try:
+            values_list = response.rstrip('\n').split('\n')[1:]
+        except IndexError:
+            raise ClientError()
 
         data_dict = {}
         for value in values_list:
             metric_value = value.split(' ')
 
-            metric_value[1] = float(metric_value[1])
-            metric_value[2] = int(metric_value[2])
+            try:
+                metric_value[1] = float(metric_value[1])
+                metric_value[2] = int(metric_value[2])
+            except (IndexError, ValueError):
+                raise ClientError()
 
             if metric_value[0] not in data_dict:
                 data_dict[metric_value[0]] = [(metric_value[2], metric_value[1])]
@@ -61,19 +95,20 @@ class Client:
                 data_dict[metric_value[0]].append((metric_value[2], metric_value[1]))
 
         for value in data_dict.values():
-            sorted(value, key=operator.itemgetter(1))
+            value.sort(key=operator.itemgetter(0))
 
         return data_dict
 
-    def check_response(self):
-        pass
+    @staticmethod
+    def check_request_format_client(command: str):
+        command_split = command.split(' ')
+
+        if len(command_split) != 4:
+            return False
+        elif not command_split[3].endswith('\n'):  # check \n
+            return False
+        elif command_split[0] not in COMMANDS:
+            return False
 
     def close_connection(self):
         self.connection_sock.close()
-
-
-# if __name__ == '__main__':
-#     client = Client('127.0.0.1', 8888)
-#     # client.put('hello.cpu', 12)
-#     print(client.get('*'))
-#     client.close_connection()
